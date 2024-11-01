40a63be63ddad714a10dcc63c56626cee20b5924
tamanna-kumari <t7788kumari@gmail.com>
2024-11-01 12:16:58 -0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# Main configuration headers
+
+LOG_FILE_PATH = "key_logger.txt"
+SENDER_EMAIL = "xyz@gmail.com"
+RECEIVER_EMAIL = "abc@gmail.com"
+EMAIL_PASSWORD = "password_here"
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
